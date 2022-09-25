31f3c698022942dcfa2c6db7dc86ffef355db593
toh995 <toh995@gmail.com>
2022-09-26 00:11:19 +0900

    change single quotes to double quotes

--- .config/nvim/ftplugin/help.lua
+++ .config/nvim/ftplugin/help.lua
@@ -1,5 +1,5 @@
 -- always open help files in a vertical split on the far-right
-vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-  	command = 'wincmd L',
-  	pattern = { '<buffer>' },
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  	command = "wincmd L",
+  	pattern = { "<buffer>" },
 })
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,16 +12,16 @@ set suffixesadd+=.js,.ts
 -- OPTIONS --
 -------------
 -- map the leader key
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 -- make all copy/paste operations use the system clipboard
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 
 -- ignore case when searching
 vim.opt.ignorecase = true
 
 -- mouse support
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- show line numbers
 vim.opt.number = true
@@ -34,16 +34,16 @@ vim.opt.splitright = true
 ------------------
 -- automatically detect file changes
 vim.opt.updatetime = 500
-vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'CursorHoldI', 'FocusGained' }, {
-  	pattern = { '*' },
-  	command = 'checktime',
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  	pattern = { "*" },
+  	command = "checktime",
 })
 
 -- disable the auto-insertion of comments
-vim.api.nvim_create_autocmd({ 'FileType' }, {
-	pattern = { '*' },
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "*" },
 	callback = function()
-		vim.opt_local.formatoptions:remove({ 'c', 'r', 'o' })
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
 	end
 })
 
@@ -51,10 +51,10 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
 -- KEYBOARD SHORTCUTS --
 ------------------------
 -- set the keyboard shortcut ctrl+a to select all
-vim.api.nvim_set_keymap('', '<C-a>', '<esc>ggVG<CR>', { noremap = true })
+vim.api.nvim_set_keymap("", "<C-a>", "<esc>ggVG<CR>", { noremap = true })
 
 -- re-map keys to navigate through splits
-vim.api.nvim_set_keymap('', '<C-J>', '<C-W><C-J>', { noremap = true })
-vim.api.nvim_set_keymap('', '<C-K>', '<C-W><C-K>', { noremap = true })
-vim.api.nvim_set_keymap('', '<C-L>', '<C-W><C-L>', { noremap = true })
-vim.api.nvim_set_keymap('', '<C-H>', '<C-W><C-H>', { noremap = true })
+vim.api.nvim_set_keymap("", "<C-J>", "<C-W><C-J>", { noremap = true })
+vim.api.nvim_set_keymap("", "<C-K>", "<C-W><C-K>", { noremap = true })
+vim.api.nvim_set_keymap("", "<C-L>", "<C-W><C-L>", { noremap = true })
+vim.api.nvim_set_keymap("", "<C-H>", "<C-W><C-H>", { noremap = true })
