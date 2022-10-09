3cebd9e6ab013709a39cdd0234c2e87b536c658c
toh995 <toh995@gmail.com>
2022-10-10 00:57:13 +0900

    neovim, additional nvim-tree config

--- .config/nvim/lua/plugin_config/nvim_tree.lua
+++ .config/nvim/lua/plugin_config/nvim_tree.lua
@@ -1,8 +1,28 @@
 -- @module plugin_config.nvim_tree
 local nvim_tree = {}
 
+local nt = require('nvim-tree')
+local nt_api = require('nvim-tree.api')
+
+local on_attach
+
 function nvim_tree.configure()
-	require("nvim-tree").setup()
+	nt.setup({
+		actions = {
+			open_file = {
+				window_picker = {
+					enable = false,
+				},
+			},
+		},
+
+		on_attach = on_attach
+	})
 end
+
+function on_attach(_)
+	vim.keymap.set("", "e", function() nt_api.node.open.vertical() end)
+end
+
 
 return nvim_tree
