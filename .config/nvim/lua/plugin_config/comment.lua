6d6c84ef296396a06fd84581b363dd15ab034a9b
toh995 <toh995@gmail.com>
2022-10-10 18:55:56 +0900

    neovim, remove custom comment config for .hbs

--- .config/nvim/lua/plugin_config/comment.lua
+++ .config/nvim/lua/plugin_config/comment.lua
@@ -2,13 +2,9 @@
 local comment = {}
 
 local c = require("Comment")
-local ft = require('Comment.ft')
 
 function comment.configure()
 	c.setup()
-
-	-- set a custom comment string for handlebars
-	ft.set("handlebars", { "{{!--%s--}}", "{{!--%s--}}" })
 end
 
 return comment
