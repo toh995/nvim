19ec2ced75ba9f149547a66add7f76a1c1773124
toh995 <toh995@gmail.com>
2022-10-16 19:13:19 +0900

    neovim, clean up/refactor lsp configs

---
+++ .config/nvim/lua/plugin_config/lsp/init.lua
@@ -0,0 +1,96 @@
+-- @module plugin_config.lsp
+local lsp = {}
+
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+local mason = require("mason")
+local mason_lspconfig = require("mason-lspconfig")
+local mason_null_ls = require("mason-null-ls")
+local null_ls = require("null-ls")
+
+local server_configs = require("plugin_config.lsp.server_configs")
+
+local configure_autocomplete
+local configure_null_ls
+
+function lsp.configure()
+	-- set up mason FIRST
+	mason.setup()
+	mason_lspconfig.setup({ automatic_installation = true })
+
+	configure_autocomplete()
+	configure_null_ls()
+	server_configs.configure()
+end
+
+function configure_autocomplete()
+	if cmp == nil then
+		return
+	else
+		cmp.setup({
+			sources = {
+				{ name = "buffer" },
+				{ name = "luasnip" },
+				{ name = "nvim_lsp" },
+				{ name = "nvim_lsp_signature_help" },
+				{ name = "path" },
+			},
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+		})
+	end
+end
+
+function configure_null_ls()
+	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	null_ls.setup({
+		sources = {
+			null_ls.builtins.formatting.eslint_d.with({
+				extra_args = { "--report-unused-disable-directives", "--fix" },
+			}),
+			null_ls.builtins.formatting.prettierd,
+			null_ls.builtins.formatting.stylua,
+		},
+		on_attach = function(client, bufnr)
+			if client.supports_method("textDocument/formatting") then
+				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = augroup,
+					buffer = bufnr,
+					callback = function()
+						vim.lsp.buf.format({
+							bufnr = bufnr,
+							filter = function(c)
+								return c.name == "null-ls"
+							end,
+						})
+					end,
+				})
+			end
+		end,
+	})
+
+	mason_null_ls.setup({ automatic_installation = true })
+end
+
+return lsp
---
+++ .config/nvim/lua/plugin_config/lsp/server_configs.lua
@@ -0,0 +1,95 @@
+-- @module plugin_config.lsp.server_configs
+local server_configs = {}
+
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+local lspconfig = require("lspconfig")
+
+local capabilities = cmp_nvim_lsp.default_capabilities()
+
+local function set_lsp_keybindings(_, bufnr)
+	vim.keymap.set("", "<leader>gd", vim.lsp.buf.definition, { noremap = true, buffer = bufnr })
+	vim.keymap.set("", "<leader>rn", vim.lsp.buf.rename, { noremap = true, buffer = bufnr })
+	vim.keymap.set("", "<leader>gr", vim.lsp.buf.references, { noremap = true, buffer = bufnr })
+	vim.keymap.set("", "<leader>k", vim.lsp.buf.hover, { noremap = true, buffer = bufnr })
+end
+
+function server_configs.configure()
+	---------
+	-- CSS --
+	---------
+	lspconfig.cssls.setup({
+		capabilities = capabilities,
+		on_attach = set_lsp_keybindings,
+	})
+
+	-----------
+	-- Ember --
+	-----------
+	lspconfig.ember.setup({
+		capabilities = capabilities,
+		on_attach = set_lsp_keybindings,
+	})
+
+	-------------
+	-- ES Lint --
+	-------------
+	lspconfig.eslint.setup({
+		capabilities = capabilities,
+		on_attach = set_lsp_keybindings,
+	})
+
+	---------
+	-- GLint --
+	---------
+	lspconfig.glint.setup({
+		capabilities = capabilities,
+		on_attach = set_lsp_keybindings,
+	})
+
+	----------
+	-- HTML --
+	----------
+	lspconfig.html.setup({
+		capabilities = capabilities,
+		on_attach = set_lsp_keybindings,
+		filetypes = { "html", "handlebars" },
+	})
+
+	---------
+	-- Lua --
+	---------
+	lspconfig.sumneko_lua.setup({
+		capabilities = capabilities,
+		on_attach = set_lsp_keybindings,
+		settings = {
+			Lua = {
+				runtime = {
+					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+					version = "LuaJIT",
+				},
+				diagnostics = {
+					-- Get the language server to recognize the `vim` global
+					globals = { "vim" },
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+				telemetry = {
+					-- Do not send telemetry data containing a randomized but unique identifier
+					enable = false,
+				},
+			},
+		},
+	})
+
+	---------------------------
+	-- TypeScript/JavaScript --
+	---------------------------
+	lspconfig.tsserver.setup({
+		capabilities = capabilities,
+		on_attach = set_lsp_keybindings,
+	})
+end
+
+return server_configs
