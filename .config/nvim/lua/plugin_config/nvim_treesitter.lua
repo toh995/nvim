56f1f67de867996983bac631e75b40c5b0c56c5b
toh995 <toh995@gmail.com>
2022-10-16 23:26:08 +0900

    neovim, add folding via treesitter

--- .config/nvim/lua/plugin_config/nvim_treesitter.lua
+++ .config/nvim/lua/plugin_config/nvim_treesitter.lua
@@ -4,6 +4,12 @@ local nvim_treesitter = {}
 local configs = require("nvim-treesitter.configs")
 
 function nvim_treesitter.configure()
+	-- use treesitter for folding
+	vim.opt.foldmethod = "expr"
+	vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+	-- ensure that newly opened buffers start with all folds open
+	vim.opt.foldlevelstart = 99
+
 	configs.setup({
 		-- Automatically install missing parsers when entering buffer
 		auto_install = true,
