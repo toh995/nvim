818c8949c3fbb02c9590a715f2278d991e2ab693
toh995 <toh995@gmail.com>
2022-10-17 23:38:39 +0900

    neovim, set the gitconfig comment string to #

--- .config/nvim/lua/plugin_config/comment.lua
+++ .config/nvim/lua/plugin_config/comment.lua
@@ -2,9 +2,14 @@
 local comment = {}
 
 local c = require("Comment")
+local ft = require("Comment.ft")
 
 function comment.configure()
 	c.setup()
+
+	-- override the default gitconfig comment string
+	-- (the default is also correct, this is just personal preference)
+	ft.gitconfig = { "# %s" }
 end
 
 return comment
