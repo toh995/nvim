ab0461f943a552c7a5acd872d2648666fb8b788c
toh995 <toh995@gmail.com>
2022-10-17 23:49:51 +0900

    neovim, configure date display for git blame

--- .config/nvim/lua/plugin_config/git_blame.lua
+++ .config/nvim/lua/plugin_config/git_blame.lua
@@ -7,6 +7,10 @@ function git_blame.configure()
 	-- disable gitblame on startup
 	-- vim.g.gitblame_enabled = 0
 
+	-- configure date format
+	-- reference: https://github.com/f-person/git-blame.nvim#date-format
+	vim.g.gitblame_date_format = "%a %Y-%m-%d %I:%M %p"
+
 	-- keybindings
 	-- vim.keymap.set("", "<leader>gb", function() vim.api.nvim_command("GitBlameToggle") end, { noremap = true })
 	vim.keymap.set("", "<leader>gbu", gb.open_commit_url, { noremap = true })
