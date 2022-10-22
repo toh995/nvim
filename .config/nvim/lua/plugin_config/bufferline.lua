e06518ae9dcfcfc0b9000cf8640ee58d5a868ac4
toh995 <toh995@gmail.com>
2022-10-23 08:51:30 +0900

    neovim, additional keyboard shortcuts for new tabs

--- .config/nvim/lua/plugin_config/bufferline.lua
+++ .config/nvim/lua/plugin_config/bufferline.lua
@@ -6,11 +6,15 @@ local b = require("bufferline")
 function bufferline.configure()
 	-- set up some keyboard shortcuts for tabs
 	vim.keymap.set("", "t", ":tabnew<CR>", { noremap = true })
+	vim.keymap.set("", "T", "<C-W>T", { noremap = true })
 	vim.keymap.set("", "Q", ":tabclose<CR>", { noremap = true })
 	vim.keymap.set("", "J", ":BufferLineCyclePrev<CR>", { noremap = true })
 	vim.keymap.set("", "K", ":BufferLineCycleNext<CR>", { noremap = true })
 	vim.keymap.set("", "<leader>J", ":BufferLineMovePrev<CR>", { noremap = true })
 	vim.keymap.set("", "<leader>K", ":BufferLineMoveNext<CR>", { noremap = true })
+
+	-- user commands
+	vim.api.nvim_create_user_command("TVS", "tab vs", {})
 
 	b.setup({
 		options = {
