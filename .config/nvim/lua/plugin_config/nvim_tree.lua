c8dd291b26566a3d8c126a662cedb7cef296049e
toh995 <toh995@gmail.com>
2023-05-01 17:26:59 -0700

    neovim, remove deprecated nvim-tree option `open_on_setup`

--- .config/nvim/lua/plugin_config/nvim_tree.lua
+++ .config/nvim/lua/plugin_config/nvim_tree.lua
@@ -5,11 +5,11 @@ local nt = require("nvim-tree")
 local api = require("nvim-tree.api")
 
 local on_attach
+local open_nvim_tree
 
 function nvim_tree.configure()
 	-- setup
 	nt.setup({
-		open_on_setup = true,
 		open_on_tab = true,
 		on_attach = on_attach,
 		git = { ignore = false },
@@ -29,10 +29,30 @@ function nvim_tree.configure()
 	vim.keymap.set("", "<leader>nt", function()
 		api.tree.toggle(true)
 	end, { noremap = true })
+
+	-- open nvim tree on startup
+	vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 end
 
 function on_attach(_)
 	-- vim.keymap.set("", "e", function() api.node.open.vertical() end, { noremap = true })
+end
+
+-- open the tree if startup buffer is a directory, is empty or is unnamed
+function open_nvim_tree(data)
+	-- buffer is a [No Name]
+	local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+	-- buffer is a directory
+	local directory = vim.fn.isdirectory(data.file) == 1
+	if not no_name and not directory then
+		return
+	end
+	-- change to the directory
+	if directory then
+		vim.cmd.cd(data.file)
+	end
+	-- open the tree
+	require("nvim-tree.api").tree.open()
 end
 
 return nvim_tree
