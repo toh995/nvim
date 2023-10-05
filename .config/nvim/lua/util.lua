264e87de96cb7d6a4e041b1db56ca77f7834c728
toh995 <52012721+toh995@users.noreply.github.com>
2023-10-04 22:05:34 -0700

    neovim, add util.clone_deep

--- .config/nvim/lua/util.lua
+++ .config/nvim/lua/util.lua
@@ -15,4 +15,18 @@ function util.tbl_keys(tbl)
 	return ret
 end
 
+function util.clone_deep(val)
+	-- Adapted from https://gist.github.com/tylerneylon/81333721109155b2d244
+	if type(val) ~= "table" then
+		return val
+	end
+
+	local ret = {}
+	for k, v in pairs(val) do
+		ret[util.clone_deep(k)] = util.clone_deep(v)
+	end
+
+	return ret
+end
+
 return util
