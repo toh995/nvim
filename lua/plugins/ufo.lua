3a47a8dcc32a167716ffa07e14356ebd0a6d30e1
toh995 <52012721+toh995@users.noreply.github.com>
2024-01-21 00:13:55 -0800

    disable folds on aerial and nvim-tree

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -4,6 +4,7 @@ local M = {}
 function M.config()
 	local ufo = require("ufo")
 
+	local constFt = require("const.filetypes")
 	local user_icons = require("const.user_icons")
 
 	vim.o.foldcolumn = "1" -- '0' is not bad
@@ -18,12 +19,30 @@ function M.config()
 		foldclose = user_icons.chevron.Right,
 	})
 
+	-- Disable folds on certain filetypes
+	local disabled_fts = {
+		[constFt.Aerial] = true,
+		[constFt.NvimTree] = true,
+	}
+	vim.api.nvim_create_autocmd({ "BufEnter" }, {
+		callback = function(opts)
+			local ft = vim.bo[opts.buf].filetype
+			local is_disabled = disabled_fts[ft]
+			if is_disabled then
+				vim.opt_local.foldenable = false
+			end
+		end,
+	})
+
+	-- Keymaps
 	vim.keymap.set("n", "<leader>h", "zc") -- close fold
 	vim.keymap.set("n", "<leader>l", "zo") -- open fold
 	vim.keymap.set("n", "<leader>H", ufo.closeAllFolds)
 	vim.keymap.set("n", "<leader>L", ufo.openAllFolds)
 
+	-- Set up UFO
 	-- See https://github.com/kevinhwang91/nvim-ufo/issues/4#issuecomment-1512772530
+	---@diagnostic disable-next-line: missing-fields
 	ufo.setup({
 		-- Disable highlights, when opening a fold
 		open_fold_hl_timeout = 0,
