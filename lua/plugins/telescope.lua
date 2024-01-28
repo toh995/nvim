01fc0bf1779f39394c7a9c498febc0c6f2333165
toh995 <52012721+toh995@users.noreply.github.com>
2024-01-27 20:04:50 -0800

    refactor telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,26 @@
 -- @module plugins.telescope
 local M = {}
 
+local set_autocmds
+local set_usercmds
+
 function M.config()
-	local actions = require("telescope.actions")
-	local builtin = require("telescope.builtin")
-	local telescope = require("telescope")
-	local nvim_tree_api = require("nvim-tree.api")
+	local pkgs = {
+		actions = require("telescope.actions"),
+		builtin = require("telescope.builtin"),
+		nvim_tree_api = require("nvim-tree.api"),
+		telescope = require("telescope"),
+	}
 
-	--[[ 
-  https://github.com/nvim-telescope/telescope.nvim/wiki/Extensions
-
-  todo:
-    - builtin.quickfix 
-    - builtin.quickfixhistory
-    - builtin.oldfiles
-    - builtin.colorscheme
-  --]]
-	-- setup
-	telescope.setup({
+	-- Set up telescope
+	pkgs.telescope.setup({
 		defaults = {
 			cache_picker = { num_pickers = 20 },
 			mappings = {
 				i = {
 					["<C-u>"] = false,
-					["<C-j>"] = actions.move_selection_next,
-					["<C-k>"] = actions.move_selection_previous,
+					["<C-j>"] = pkgs.actions.move_selection_next,
+					["<C-k>"] = pkgs.actions.move_selection_previous,
 				},
 			},
 		},
@@ -38,9 +34,14 @@ function M.config()
 			},
 		},
 	})
-	telescope.load_extension("fzf")
 
-	-- autocommands
+	pkgs.telescope.load_extension("fzf")
+
+	set_autocmds()
+	set_usercmds(pkgs)
+end
+
+function set_autocmds()
 	local augroup = vim.api.nvim_create_augroup("telescope_custom", { clear = true })
 
 	-- Show line numbers in the preview window!
@@ -52,33 +53,38 @@ function M.config()
 			vim.wo.number = true
 		end,
 	})
+end
 
-	-- Reopen previous telescope window
-	vim.api.nvim_create_user_command("R", builtin.resume, {})
-	vim.api.nvim_create_user_command("RS", builtin.pickers, {})
+function set_usercmds(pkgs)
+	-- Reopen previous telescope windows
+	-- Single
+	vim.api.nvim_create_user_command("R", pkgs.builtin.resume, {})
+	-- Multiple
+	vim.api.nvim_create_user_command("RS", pkgs.builtin.pickers, {})
 
 	-- Aerial
-	telescope.load_extension("aerial")
-	vim.api.nvim_create_user_command("S", telescope.extensions.aerial.aerial, {})
+	pkgs.telescope.load_extension("aerial")
+	vim.api.nvim_create_user_command("S", pkgs.telescope.extensions.aerial.aerial, {})
 
 	-- LSP diagnostics
+	-- Current buffer only
 	vim.api.nvim_create_user_command("D", function()
-		-- fetch for current buffer only
-		builtin.diagnostics({ bufnr = 0 })
+		pkgs.builtin.diagnostics({ bufnr = 0 })
 	end, {})
+
+	-- ALL buffers
 	vim.api.nvim_create_user_command("DA", function()
-		-- fetch for ALL buffers
-		builtin.diagnostics({ bufnr = nil })
+		pkgs.builtin.diagnostics({ bufnr = nil })
 	end, {})
 
 	-- File picker
 	vim.api.nvim_create_user_command("F", function()
-		builtin.find_files({
+		pkgs.builtin.find_files({
 			hidden = true,
 			attach_mappings = function()
-				actions.select_default:enhance({
+				pkgs.actions.select_default:enhance({
 					post = function()
-						nvim_tree_api.tree.find_file({})
+						pkgs.nvim_tree_api.tree.find_file({})
 					end,
 				})
 				return true
@@ -91,14 +97,14 @@ function M.config()
 		local filepath = tbl.fargs[1] or "."
 		local glob_pattern = tbl.fargs[2] or ""
 
-		builtin.live_grep({
+		pkgs.builtin.live_grep({
 			search_dirs = { filepath },
 			glob_pattern = glob_pattern,
 			prompt_title = "Grep in " .. filepath .. " " .. glob_pattern,
 			attach_mappings = function()
-				actions.select_default:enhance({
+				pkgs.actions.select_default:enhance({
 					post = function()
-						nvim_tree_api.tree.find_file({})
+						pkgs.nvim_tree_api.tree.find_file({})
 					end,
 				})
 				return true
