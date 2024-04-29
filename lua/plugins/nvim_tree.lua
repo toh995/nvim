698e2b180e2a1e33dc2856213bfadcc38f08a7b6
toh995 <52012721+toh995@users.noreply.github.com>
2024-04-28 21:07:29 -0700

    fix keymaps for nvim-tree

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -63,12 +63,16 @@ function setup_tree(pkgs)
 			-- Set default keymappings
 			pkgs.api.config.mappings.default_on_attach(bufnr)
 
-			-- Remove J and K
-			-- They're reserved for tab navigation
+			-- Remove some keymappings
+			-- They're reserved for navigation
 			vim.keymap.del("n", "J", { buffer = bufnr })
 			vim.keymap.del("n", "K", { buffer = bufnr })
+			vim.keymap.del("n", "<C-k>", { buffer = bufnr })
 
-			-- Add navigation keymaps
+			-- Show file details
+			vim.keymap.set("n", "<leader>k", pkgs.api.node.show_info_popup, opts("Info"))
+
+			-- Add tree navigation keymaps
 			vim.keymap.set("n", "h", pkgs.api.node.navigate.parent_close, opts("Collapse current folder"))
 			vim.keymap.set("n", "H", pkgs.api.tree.collapse_all, opts("Collapse All"))
 			vim.keymap.set("n", "l", pkgs.api.node.open.edit, opts("Open"))
