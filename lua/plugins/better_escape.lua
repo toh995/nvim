49388603a51b1a3f6b471a26a373abaa0703d355
toh995 <52012721+toh995@users.noreply.github.com>
2024-07-17 00:46:57 -0700

    update better_escape plugin

--- lua/plugins/better_escape.lua
+++ lua/plugins/better_escape.lua
@@ -1,12 +1,25 @@
 -- @module plugins.better_escape
 local M = {}
 
+-- Export for usage elsewhere
 M.keystring = "kj"
+
+-- Copied from https://github.com/max397574/better-escape.nvim?tab=readme-ov-file#rewrite
+local function handle_escape()
+	vim.api.nvim_input("<esc>")
+	local current_line = vim.api.nvim_get_current_line()
+	if current_line:match("^%s+j$") then
+		vim.schedule(function() vim.api.nvim_set_current_line("") end)
+	end
+end
 
 function M.config()
 	require("better_escape").setup({
-		mapping = { M.keystring },
-		clear_empty_lines = true,
+		mappings = {
+			i = {
+				k = { j = handle_escape },
+			},
+		},
 	})
 end
 
