3c09a8664481320bc752a3ca913fdfa29117be74
toh995 <52012721+toh995@users.noreply.github.com>
2026-08-08 19:29:45 -0700

    add lean support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -52,6 +52,15 @@ function M.configure()
 				},
 			},
 		},
+		-- Lean
+		{
+			"Julian/lean.nvim",
+			event = { "BufReadPre *.lean", "BufNewFile *.lean" },
+			dependencies = { "nvim-telescope/telescope.nvim" },
+			-- init = function()
+			-- 	vim.g.lean_config = { mappings = true }
+			-- end,
+		},
 
 		-- Auto-complete
 		{
