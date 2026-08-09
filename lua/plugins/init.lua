3124c1064df3a0e3b65367deff63e56661ac6dab
toh995 <52012721+toh995@users.noreply.github.com>
2026-08-08 19:30:12 -0700

    add oil.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -167,6 +167,16 @@ function M.configure()
 				timeout_ms = 60000,
 			},
 		},
+		{
+			"stevearc/oil.nvim",
+			---@type oil.SetupOpts
+			opts = {
+				default_file_explorer = false,
+			},
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+			lazy = false,
+		},
 
 		-- Fuzzy finder
 		{
